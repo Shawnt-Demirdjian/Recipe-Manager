e7ad9c7978f08457aff475c0492b63cf0d851cdb
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2021-02-21 01:29:52 -0800

    Align Java versions of docker container and pom. Add CORS allow everywhere.

--- api/src/main/java/com/demirdjian/recipemanager/RecipeManagerApplication.java
+++ api/src/main/java/com/demirdjian/recipemanager/RecipeManagerApplication.java
@@ -2,6 +2,9 @@ package com.demirdjian.recipemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipeManagerApplication {
@@ -13,6 +16,21 @@ public class RecipeManagerApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeManagerApplication.class, args);
+	}
+
+	/**
+	 * Configures the CORS allowed Origins properties.
+	 * 
+	 * @return WebMvcConfigurer
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
